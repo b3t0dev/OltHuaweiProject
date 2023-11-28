1cf723181b93c550dd3ff32fe81f50e427b411b3
b3t0dev <101339834+b3t0dev@users.noreply.github.com>
2023-11-27 21:03:50 -0300

    Update index.js

--- BackEnd/SSHManager/index.js
+++ BackEnd/SSHManager/index.js
@@ -18,7 +18,8 @@ class SSHManager {
     this.hostname = 'MA5800-X17';
     this.regex = [];
     this.matchCommand = 0;
-    this.commandCounter = 0;   
+    this.commandCounter = 0;  
+    this.commandError = 'Unknown command, the error' 
     this.commandsQueue = [];
   }
 
@@ -42,12 +43,18 @@ class SSHManager {
   }
 
   addToQueue(comandos, callback) {
-
+    
+    const regInterfaceCommand = 'interface gpon [0-9]{1,}\/.{1,}' // Regex pra verificar se existe algum comando intermed
     const preComand = 'en\nconfig\n'
     const posComand = ' | no-more\n\n'
     let command = '';
     command += preComand;
-
+    
+    if ( comandos[0].match(regInterfaceCommand) ) {
+      command += '\n' + comandos[0] + '\n';
+      comandos.shift();
+    }  
+    
     for (let cmd = 0; cmd < comandos.length; cmd++) {
       command += comandos[cmd] + posComand;
       this.regex.push(comandos[cmd]);
@@ -72,25 +79,43 @@ class SSHManager {
         preout = 'OUTPUT: ' + String(data); // O Stdout é retornado um Object()
         // console.log(preout); // Ativar para exibir todas as saidas (For DeBug)
 
+        if (preout.match(this.commandError)){
+          const errorSaida = 'Comando não encontrado'
+          output.push(errorSaida)
+          callback(null, output);
+          // this.disconnect();
+        }
+
         if ( this.matchCommand == 1 ){
-          output.push(`Saida do Comando [${this.commandCounter}]: ` + data);
+          let saida = String(data).replace(/-{2,}(?![^\(]*\))/g, '') + 'END!';
+          
+          const regexOut = `\r\n${this.hostname}`
+          const regexOut2 = '(^ {1,}\r\nEND!)|(^ {1,}END!)';
+          const regexOut3 = ' {1,}'
+          if ( !saida.match(regexOut) && !saida.match(regexOut2) ) {
+            saida = saida.replace('\r\nEND!', '');
+            saida = saida.replace('END!', '');
+            if (saida != '') {
+              output.push(saida);
+            }
+          }
+
           if (preout.match(this.hostname + '.{1,}')){
             this.regex.shift();
             this.matchCommand = 0;
           }
         }
 
-        if ( this.regex[0] && preout.match(this.regex[0]) ){
+        if ( (this.regex[0] && preout.match(this.regex[0])) ){
           this.matchCommand = 1;
           this.commandCounter += 1;
         }
         if ( preout.match(this.hostname + '.{1,}') && ( this.regex.length == 0 ) ){
-          // console.log(output);
+          // console.log(output)
           callback(null, output);
           this.disconnect();
         }
 
-        
         });
         
       stream.end(command);
@@ -109,7 +134,7 @@ const oltA_config = {
 
 // Criando objeto(OLT) passando as configurações para a classe executar os métodos
 const oltA = new SSHManager( oltA_config );
-const comandos = ['display ont autofind all','display sysuptime'];
+const comandos = ['display optversion'];
 oltA.connect();
 
 // Executar o comando
